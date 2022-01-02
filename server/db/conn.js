fb1b0859e0c954526d04dd6b2cc85d78062fe889
Jay Flanary <j.c.flanary@gmail.com>
2022-01-01 21:31:12 -0500

    heroku update

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,7 +1,4 @@
 const Sequelize = require('sequelize');
-const pg = require('pg');
-
-pg.defaults.ssl = true;
 
 const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:Bigman<26@localhost:5432/acme-movies');
 
