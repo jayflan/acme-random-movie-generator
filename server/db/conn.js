eab2fb7aca809a711ebec668124a243540989445
Jay Flanary <j.c.flanary@gmail.com>
2022-01-05 09:26:54 -0500

    more heroku updates

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,6 +1,21 @@
 const Sequelize = require('sequelize');
 
-let conn = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:Bigman<26@localhost:5432/acme-movies');
+let conn = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:Bigman<26@localhost:5432/acme-movies'
+// {
+//   host: 'ec2-34-236-87-247.compute-1.amazonaws.com',
+//   dialect: 'postgres',
+//   ssl: true,
+//   protocol: "postgres",
+
+//   logging: true,
+//   dialectOptions: {
+//       ssl: {
+//           require: true,
+//           rejectUnauthorized: false // <<<<<< YOU NEED THIS
+//       }
+//   }
+// }
+);
 
 conn
     .authenticate()
