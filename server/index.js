d6a453b84048b0e6e0ca666c7af25ffa5d7b3ae1
Jay Flanary <j.c.flanary@gmail.com>
2022-01-04 21:57:44 -0500

    heroku update

--- server/index.js
+++ server/index.js
@@ -1,12 +1,8 @@
+const conn = require('./db/conn')
 const app = require('./app');
-const db = require('./db');
 const { syncAndSeed } = require('./db/index')
 const PORT = process.env.PORT || 3000;
 
-const Sequelize = require('sequelize');
-
-// const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:Bigman<26@localhost:5432/acme-movies');
-// module.exports = conn;
 const init = async() => {
   await syncAndSeed();
   app.listen(PORT, ()=> console.log(`listening on port ${PORT}`));
