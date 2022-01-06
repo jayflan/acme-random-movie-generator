80fc2031fa572cd395392c42d54401e05244652d
Jay Flanary <j.c.flanary@gmail.com>
2022-01-05 19:58:19 -0500

    clean up

--- server/app.js
+++ server/app.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-// const morgan = require('morgan');
-const { Movie } = require('./db').models;
 
-//middleware
-// app.use(morgan('dev'));
 
 //req body parsing middleware is important for updating!!!!!!
 app.use(express.json()); // Remember!!!: Don't forget ()!!!!
@@ -17,18 +13,8 @@ app.use('/dist', express.static(path.join(__dirname, '..','dist')));
 
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '..', 'client','index.html')));
 
-app.use('/api', require('./api')); //<--not working, switched api test/views into app.js
-  //found this in websocket workshop, isn't working there either?
-//api test views
-// app.get('/api', async(req, res, next) => {
-//   try {
-//     res.send(await Movie.findAll());
-//   }
-//   catch(err) {
-//     next(err)
-//   }
-// })
-
+app.use('/api', require('./api'));
+  
 //error handling
 app.use((req, res, next) => {
   const error = Error('Page not found');
--- server/db/conn.js
+++ server/db/conn.js
@@ -1,24 +1,6 @@
 const Sequelize = require('sequelize');
 
-let conn = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:Bigman<26@localhost:5432/acme-movies', {
-ssl: {
-    rejectUnauthorized: false
-  }
-// {
-//   host: 'ec2-34-236-87-247.compute-1.amazonaws.com',
-//   dialect: 'postgres',
-//   ssl: true,
-//   protocol: "postgres",
-
-//   logging: true,
-//   dialectOptions: {
-//       ssl: {
-//           require: true,
-//           rejectUnauthorized: false // <<<<<< YOU NEED THIS
-//       }
-//   }
-// }
-});
+let conn = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:Bigman<26@localhost:5432/acme-movies')
 
 conn
     .authenticate()
