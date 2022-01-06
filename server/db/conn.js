46a199c58e00c76e8dacaded37475b0d88fffbe1
Jay Flanary <j.c.flanary@gmail.com>
2022-01-06 08:27:07 -0500

    heroku ssl test

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,18 +1,30 @@
-const Sequelize = require('sequelize');
 
-let conn = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:Bigman<26@localhost:5432/acme-movies', {
-    dialectOptions: {
-        ssl: {
-          require: true,
-          ca: fs.readFileSync(`${__dirname}/us-east-1-bundle.pem`)
+const Sequelize = require('sequelize');
+// const fs = require('fs');
+//go to this AWS page to download ssl certs for heroku deployment ssl:
+  //https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/UsingWithRDS.SSL.html
+  //place cert in same folder as this conn file
+
+let conn = '';
+
+if(process.env.DATABASE_URL) {
+    conn = new Sequelize(process.env.DATABASE_URL, {
+        dialectOptions: {
+            ssl: {  
+              require: true,
+              ca: fs.readFileSync(`${__dirname}/us-east-1-bundle.pem`)
+            }
         }
-      }
+    })
+} else {
+        conn = new Sequelize('postgres://postgres:Bigman<26@localhost:5432/acme-movies')
+}
 
     // dialectOptions: {
     //     ssl: {
     //         rejectUnauthorized: false
     //     }
     // }
-})
+// )
 
 module.exports = conn;
